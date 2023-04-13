d2df4e4f720d0c13b4770ab047570cad76a333ff
Papun Mohanty <papun.mohanty@homerload.com>
2023-04-13 20:03:07 +0530

    Add: Label Detaction Code

--- routes/vision.js
+++ routes/vision.js
@@ -1,16 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const AWS = require('aws-sdk');
+const router = require('express').Router();
+let dotenv = require('dotenv').config()
 
 router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+  // Check if file was uploaded
+  if (!req.files || !req.files.file) {
+    return res.status(400).json({error: 'File not found'});
+  }
 
-  // Your code starts here //
+  // Read image file from request buffer
+  const image = req.files.file.data;
 
-  // Your code ends here //
+  AWS.config.credentials = new AWS.Credentials({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'ap-southeast-1'
+  });
 
-  res.json({
-    "labels": response
+  // Initialize AWS Rekognition client
+  const rekognition = new AWS.Rekognition({
+    region: 'ap-southeast-1'
+  });
+
+  // Call AWS Rekognition API to detect labels in image
+  rekognition.detectLabels({
+    Image: {
+      Bytes: image
+    }
+  }, function(err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({error: 'Unable to process the request'});
+    }
+
+    // Extract labels from API response
+    const labels = data.Labels.map(label => label.Name);
+
+    // Return labels in response
+    return res.json({labels: labels});
   });
 });
 
